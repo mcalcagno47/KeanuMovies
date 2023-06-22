0a807b52e28aeb2c9c4a6efdfdc68a6c0d91a7db
Mark Calcagno <mcalcagn@gmail.com>
2023-06-22 17:38:53 -0500

    initial start, created database for keanu reeves movies

---
+++ db/query.sql
@@ -0,0 +1,6 @@
+(SELECT
+films.id AS films_id,
+films.title,
+films.release_year,
+films.synopsis
+FROM films)
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO films (title, release_year, synopsis)
+VALUES ("John Wick", 2014, "John Wick, a former hitman who is forced back into the criminal underworld he had previously abandoned.");
