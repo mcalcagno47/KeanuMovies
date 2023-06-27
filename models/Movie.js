746b8c77d1e6ec84424897b86fc330302f74c031
Mark Calcagno <mcalcagn@gmail.com>
2023-06-27 13:30:36 -0500

    wrote connection.js and created movie model

---
+++ models/Movie.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Movie extends Model {}
+
+Movie.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        year: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true, 
+        modelName: 'movie',
+    }
+);
+
+module.exports = Movie;
